29b775ec0dfa33ac400d6492baa29ad485599f3c
Jakub <kuba.czerny@poczta.fm>
2018-01-07 23:32:34 +0100

    Script for spliting data into test-train

---
+++ utils/split_data.py
@@ -0,0 +1,30 @@
+# Nagivate to data/test/ and run cmd
+# find . -name \*.jpg -type f -delete
+
+# The you're left with the folder structure and complete dataset in train/
+# Now let's subselect __% of the data and move it to test
+
+
+# This should be run from data/
+import os, shutil
+from random import shuffle
+
+ratio = 0.3
+
+path = os.getcwd()
+folders = dict((x[0].split('/')[-1], []) for x in os.walk(path+'/train/') if x[0] != path+'/train/')
+train = dict.fromkeys(folders)
+test = dict.fromkeys(folders)
+
+for f, _ in folders.iteritems():
+    imgs = os.listdir(path+'/train/'+f)
+    shuffle(imgs)
+    idx = int(round(len(imgs)*ratio))
+    train[f] = imgs[idx:]
+    test[f] = imgs[:idx]
+
+for f, imgs in test.iteritems():
+    for img in imgs:
+        src = path + '/train/' + f + '/' + img
+        dst = path + '/test/'  + f + '/' + img
+        shutil.move(src, dst)
